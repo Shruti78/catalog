da892397c38ab337d90bab29801895e309a21d09
Shruti <imt_2021094@iiitm.ac.in>
2022-07-24 12:22:49 +0530

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:catalog/pages/home_page.dart';
 import 'package:catalog/pages/login_page.dart';
-
-// ignore: unused_import, depend_on_referenced_packages
+import 'package:catalog/utils/routes.dart';
+import 'package:catalog/widgets/theme.dart';
+// ignore: depend_on_referenced_packages
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,14 +19,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      debugShowCheckedModeBanner: false,
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => Homepage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => Homepage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
